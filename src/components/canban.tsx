c49d4e8a463ad5e68e7fa95b8d8be4fc12f962d9
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2024-01-04 15:54:43 +0100

    adds canban-board component

---
+++ src/components/canban.tsx
@@ -0,0 +1,27 @@
+function KanbanBoard() {
+    return (
+        <div className="
+          bg-black
+          p-5
+        ">
+            <h1>KanBanBoard</h1>
+            <button
+              className="
+                rounded-lg
+                bg-grey
+                cursor-pointer
+                border-2
+                border-contrastLight
+                p-1
+                m-2
+                ring-rose-500
+                hover:ring-2
+              "
+            >
+                Button!
+            </button>
+        </div>
+    )
+}
+
+export default KanbanBoard
