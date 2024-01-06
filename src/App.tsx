eb8e1338ba43cb2e9ce8d45161a0477adba87673
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2024-01-07 00:00:06 +0100

    div

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './output.css'
-import KanbanBoard from './components/canban'
+import KanbanBoard from './components/canban-board'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <KanbanBoard />
